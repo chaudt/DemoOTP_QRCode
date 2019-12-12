40051b3fb381de9ebc1bd9b9b8c84f78f396f692
chaudt <chaudt@saigonnewport.com.vn>
2019-12-12 15:46:15 +0700

    add new project

---
+++ WebApplication4/WebApplication4/Controllers/QRCoderController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.IO;
+using WebApplication4.Security;
+
+namespace WebApplication4.Controllers
+{
+    public class QRCoderController : Controller
+    {
+        public IActionResult Index()
+        {
+            var userId = 135;
+            var pass = "123@abc";
+            var encrypt = StringCipher.Encrypt(userId.ToString(), pass);
+            var url = $"http://localhost:50001/QRCoder/OTP?md5={encrypt}";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return View(BitmapToBytes(qrCodeImage));
+        }
+
+        public IActionResult OTP(string md5)
+        {
+            var pass = "123@abc";
+            var decrypt = StringCipher.Decrypt(md5, pass);
+
+            int.TryParse(decrypt, out int userId);
+            ViewBag.OTP = TCISOTP.OTP(userId, OTPMinuteTypes.EXTRA_SMALL);
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Index(string qrText)
+        //{
+        //    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        //    QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+        //    QRCode qrCode = new QRCode(qrCodeData);
+        //    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+        //    return View(BitmapToBytes(qrCodeImage));
+        //}
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+
+        //public IActionResult GenerateFile()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public IActionResult GenerateFile(string qrText)
+        //{
+        //    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        //    QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+        //    string fileGuid = Guid.NewGuid().ToString().Substring(0, 4);
+        //    qrCodeData.SaveRawData("wwwroot/qrr/file-" + fileGuid + ".qrr", QRCodeData.Compression.Uncompressed);
+
+        //    QRCodeData qrCodeData1 = new QRCodeData("wwwroot/qrr/file-" + fileGuid + ".qrr", QRCodeData.Compression.Uncompressed);
+        //    QRCode qrCode = new QRCode(qrCodeData1);
+        //    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+        //    return View(BitmapToBytes(qrCodeImage));
+        //}
+
+        //public IActionResult ViewFile()
+        //{
+        //    List<KeyValuePair<string, Byte[]>> fileData = new List<KeyValuePair<string, byte[]>>();
+        //    KeyValuePair<string, Byte[]> data;
+
+        //    string[] files = Directory.GetFiles("wwwroot/qrr");
+        //    foreach (string file in files)
+        //    {
+        //        QRCodeData qrCodeData = new QRCodeData(file, QRCodeData.Compression.Uncompressed);
+        //        QRCode qrCode = new QRCode(qrCodeData);
+        //        Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+        //        Byte[] byteData = BitmapToBytes(qrCodeImage);
+        //        data = new KeyValuePair<string, Byte[]>(Path.GetFileName(file), byteData);
+        //        fileData.Add(data);
+        //    }
+
+        //    return View(fileData);
+        //}
+    }
+
+
+
+
+}
---
+++ WebApplication4/WebApplication4/Security/TCISOTP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WebApplication4.Security
+{
+    public static class TCISOTP
+    {
+        public static int OTP(int id, OTPMinuteTypes type)
+        {
+            var dt = DateTime.Now;
+            return (id * 1001) ^ ((dt.Day * 100 + dt.Month) * 100 + dt.Hour) * 10 + FindNumberKey(dt.Minute, (int)type);
+        }
+        public static int RetrievalOTP(int otp, OTPMinuteTypes type)
+        {
+            var dt = DateTime.Now;
+            return (otp ^ (((dt.Day * 100 + dt.Month) * 100 + dt.Hour) * 10 + FindNumberKey(dt.Minute, (int)type))) / 1001;
+        }
+        private static int FindNumberKey(int currentMinutes, int percent)
+        {
+            const int TOTAL_SENCOND_ON_MINUTE = 60;
+            int ratio = TOTAL_SENCOND_ON_MINUTE / percent;
+            int i = 1;
+            while (i * ratio <= TOTAL_SENCOND_ON_MINUTE)
+            {
+                if (currentMinutes <= (i * ratio))
+                    return i;
+                i++;
+            }
+            return 0;
+        }
+    }
+    public enum OTPMinuteTypes
+    {
+        EXTRA_SMALL = 4,
+        SMALL = 6,
+        ELONGATE_SMALL = 10,
+        MEDIUM = 12,
+        LARGE = 20,
+        MEDIUM_PLUS = 30
+    }
+}
